d535e527067ff9c766eaa66209d698c130f1fc28
Yudin Nikita <yudin.nc@gmail.com>
2019-09-21 00:15:20 +0500

    verstka ver3

---
+++ locations.php
@@ -0,0 +1,89 @@
+<?php include('templates/header.php');?>
+			<!-- Page Titile Area -->
+			<section class="page-title-section section-padding">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="title-center">
+								<div class="title-middle">
+									<h2 class="page-tagline">Find a High Water Standard near you</h2>
+									<div class="page-title">High Water Standard Locations</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<section class="section-padding">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<form action="">
+							<div class="form-group is-empty">
+		                      <input type="email" value="" name="EMAIL" class="find form-control" id="address" placeholder="Type address" required="" value="New York">
+		                      <button type="submit" name="subscribe" id="subscribe" class="btn btn-common pull-right">Find</button>
+		                      <div class="clearfix"></div>
+                    		</div>
+                    	</form>
+					</div>
+				</div>
+			</div>
+		</section>
+			<!-- Page Titile Area End -->
+      <section class="section-padding section-dark">
+            <div class="container">
+              <div class="mb-5">
+                <div class="row">
+                  <div class="col-md-12 material-title-section wow animated fadeInLeft" data-wow-delay=".2s">
+                    <div class="section-title">New York</div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-5 contact-widget-section2 wow animated fadeInLeft mb-3" data-wow-delay=".2s">
+                    <p>We New Yorkers take pride in our tap water. Many credit the water for the quality of the pizza dough and bagels that New York is famous for. Some have even called it “The Champagne of Drinking Water.” But, is it true? Or is it just another urban legend? And, is your drinking water actually safe?</p>
+                    <div class="find-widget">
+                      <a href="#"><i class="material-icons mdi mdi-map-marker"></i>1938 Stillwell Ave, Brooklyn, NY 11223</a>
+                    </div>
+                    <div class="find-widget">
+                        <a href="#"><i class="material-icons mdi mdi-phone"></i> 718-407-9357</a>
+                    </div>
+                    <div class="find-widget">
+                        <a href="#"><i class="material-icons mdi mdi-email-open mr-3"></i> hws@gmail.com</a>
+                    </div>
+                    <div class="find-widget">
+                        <a href="#"><i class="material-icons mdi mdi-clock"></i> Mon to Sat: 09:30 AM - 10.30 PM</a>
+                    </div>
+                  </div>
+                  <div class="col-md-7 wow animated fadeInRight" data-wow-delay=".2s">
+                    <img style="box-shadow: 0 1px 4px rgba(0,0,0,0.26);" src="assets/images/map.png" alt="map" class="img-fluid">
+                  </div>
+                </div>
+              </div>
+            </div>
+      </section>
+		<!-- About Section End -->
+	<?php include('templates/counter.php');?>
+		<!-- Call to action Section -->
+		<section class="call-to-action-section">
+			<div class="container">
+				<div class="row justify-content-between">
+					<div class="col-md-8 col-lg-8 col-xs-12 text-left wow animated fadeInLeft" data-wow-delay=".2s">
+							<div class="h1">Call Us 718-407-9357</div>
+							<h3>Enjoy our 24/7 support</h3>
+					</div>
+					<div class="col-md-4 col-lg-4 col-xs-12 mt-4 text-right wow animated fadeInRight" data-wow-delay=".3s">
+							<a href="javascript:void(0)" class="animated4 btn btn-border"><i class="material-icons mdi mdi-phone"></i> Contact us</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Call to action Section End -->
+
+<?php include('templates/footer.php');?>
