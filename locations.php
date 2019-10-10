9644ad605f0ee7b95e653c5afe86a6cb18f6f4ac
macknoyer <yudin.nc@gmail.com>
2019-10-11 00:12:18 +0300

    ver4

--- locations.php
+++ locations.php
@@ -27,50 +27,90 @@
 					<div class="col-md-12">
 						<form action="">
 							<div class="form-group is-empty">
-		                      <input type="email" value="" name="EMAIL" class="find form-control" id="address" placeholder="Type address" required="" value="New York">
-		                      <button type="submit" name="subscribe" id="subscribe" class="btn btn-common pull-right">Find</button>
-		                      <div class="clearfix"></div>
-                    		</div>
-                    	</form>
+							  <input type="email" value="" name="EMAIL" class="find form-control" id="address" placeholder="Type address" required="" value="New York">
+							  <button type="submit" name="subscribe" id="subscribe" class="btn btn-common pull-right">Find</button>
+							  <div class="clearfix"></div>
+							</div>
+						</form>
 					</div>
 				</div>
 			</div>
 		</section>
 			<!-- Page Titile Area End -->
-      <section class="section-padding section-dark">
-            <div class="container">
-              <div class="mb-5">
-                <div class="row">
-                  <div class="col-md-12 material-title-section wow animated fadeInLeft" data-wow-delay=".2s">
-                    <div class="section-title">New York</div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-5 contact-widget-section2 wow animated fadeInLeft mb-3" data-wow-delay=".2s">
-                    <p>We New Yorkers take pride in our tap water. Many credit the water for the quality of the pizza dough and bagels that New York is famous for. Some have even called it “The Champagne of Drinking Water.” But, is it true? Or is it just another urban legend? And, is your drinking water actually safe?</p>
-                    <div class="find-widget">
-                      <a href="#"><i class="material-icons mdi mdi-map-marker"></i>1938 Stillwell Ave, Brooklyn, NY 11223</a>
-                    </div>
-                    <div class="find-widget">
-                        <a href="#"><i class="material-icons mdi mdi-phone"></i> 718-407-9357</a>
-                    </div>
-                    <div class="find-widget">
-                        <a href="#"><i class="material-icons mdi mdi-email-open mr-3"></i> hws@gmail.com</a>
-                    </div>
-                    <div class="find-widget">
-                        <a href="#"><i class="material-icons mdi mdi-clock"></i> Mon to Sat: 09:30 AM - 10.30 PM</a>
-                    </div>
-                  </div>
-                  <div class="col-md-7 wow animated fadeInRight" data-wow-delay=".2s">
-                    <img style="box-shadow: 0 1px 4px rgba(0,0,0,0.26);" src="assets/images/map.png" alt="map" class="img-fluid">
-                  </div>
-                </div>
-              </div>
-            </div>
-      </section>
-		<!-- About Section End -->
-	<?php include('templates/counter.php');?>
-		<!-- Call to action Section -->
+	  <section class="section-padding section-dark">
+			<div class="container">
+			  <div class="mb-5">
+				<div class="row">
+				  <div class="col-md-12 material-title-section wow animated fadeInLeft" data-wow-delay=".2s">
+					<div class="section-title">New York</div>
+				  </div>
+				</div>
+				<div class="row">
+				  <div class="col-md-5 contact-widget-section2 wow animated fadeInLeft mb-3" data-wow-delay=".2s">
+					<p>We New Yorkers take pride in our tap water. Many credit the water for the quality of the pizza dough and bagels that New York is famous for. Some have even called it “The Champagne of Drinking Water.” But, is it true? Or is it just another urban legend? And, is your drinking water actually safe?</p>
+					<div class="find-widget">
+					  <a href="#"><i class="material-icons mdi mdi-map-marker"></i>1938 Stillwell Ave, Brooklyn, NY 11223</a>
+					</div>
+					<div class="find-widget">
+						<a href="#"><i class="material-icons mdi mdi-phone"></i> 718-407-9357</a>
+					</div>
+					<div class="find-widget">
+						<a href="#"><i class="material-icons mdi mdi-email-open mr-3"></i> hws@gmail.com</a>
+					</div>
+					<div class="find-widget">
+						<a href="#"><i class="material-icons mdi mdi-clock"></i> Mon to Sat: 09:30 AM - 10.30 PM</a>
+					</div>
+				  </div>
+				  <div class="col-md-7 wow animated fadeInRight" data-wow-delay=".2s">
+					<img style="box-shadow: 0 1px 4px rgba(0,0,0,0.26);" src="assets/images/map.png" alt="map" class="img-fluid">
+				  </div>
+				</div>
+			  </div>
+			</div>
+	  </section>
+	  <section class="section-padding">
+
+			<div class="container">
+				<div class="row">
+				  <div class="col-md-12 material-title-section wow animated fadeInLeft" data-wow-delay=".2s">
+					<div class="section-title">Choose High Water Standard Because</div>
+						<div class="single-service-widget wow animated fadeInUp animated">
+							<div class="media d-flex align-items-center">
+							  <div class="media-img">
+								  <img src="assets/images/icons/icon1.svg" alt="">
+							  </div>
+							  <div class="media-body">
+								  <h2 class="subtitle">We value our reputation</h2>
+								  <p>High Water Standard offers our customers the most qualitative goods from domestic and foreign producers. Our main goal is to instill confidence in our partners and customers and our sustainable competitiveness, in addition to the quality of services and products offered.</p>
+							  </div>
+							</div>
+						</div>
+						<div class="single-service-widget wow animated fadeInUp animated">
+							<div class="media d-flex align-items-center">
+							  <div class="media-img">
+								  <img src="assets/images/icons/icon2.svg" alt="">
+							  </div>
+							  <div class="media-body">
+								  <h2 class="subtitle">Efficiency</h2>
+								  <p>We will quickly deliver and install our water filter systems anywhere in the city.</p>
+							  </div>
+							</div>
+						</div>
+						<div class="single-service-widget wow animated fadeInUp animated">
+							<div class="media d-flex align-items-center">
+							  <div class="media-img">
+								  <img src="assets/images/icons/icon3.svg" alt="">
+							  </div>
+							  <div class="media-body">
+								  <h2 class="subtitle">We show flexibility in relations with our clients</h2>
+								  <p>we try to be closer to them to identify and understand their needs, finding the most effective solutions to their requests. We position ourselves as the company that anticipates the needs and desires of our clients.</p>
+							  </div>
+							</div>
+						</div>
+				  	</div>
+				</div>
+			</div>
+	  </section>
 		<section class="call-to-action-section">
 			<div class="container">
 				<div class="row justify-content-between">
