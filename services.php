5d0b3cebfaf3138ee809d58d14d23ea1a7e8cb91
macknoyer <yudin.nc@gmail.com>
2019-10-11 00:02:23 +0300

    ver4

---
+++ services.php
@@ -0,0 +1,314 @@
+<?php include('templates/header.php');?>
+			<!-- Page Titile Area -->
+			<section class="page-title-section section-padding section-dark">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="title-center">
+								<div class="title-middle">
+									<h2 class="page-tagline">Install water treatment system to protect your family, the environment, and your pocketbook</h2>
+									<div class="page-title">Services</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- Page Titile Area End -->
+		<section class="material-tabs-section section-padding">
+			<div class="container">
+				<div class="row">
+					<!-- Section Titile -->
+					<div class="col-md-12 material-title-section wow animated fadeInLeft" data-wow-delay=".2s">
+						<div class="section-title">How We Can Help You?</div>
+					</div>
+				</div>
+				<div class="row">
+					<!-- Section Titile -->
+					<div class="col-12 wow animated fadeInLeft">
+						<div class="Material-tab-default">
+							<!-- Nav tabs -->
+							<ul class="nav nav-tabs futuries text-center" id="myTab" role="tablist">
+								<li class="nav-item">
+									<img src="assets/images/icons/icon2.svg" alt="Repair"><a class="nav-link active" data-toggle="tab" href="#install" role="tab">Installation</a>
+								</li>
+								<li class="nav-item">
+									<img src="assets/images/icons/icon3.svg" alt="Maintenance"><a class="nav-link" data-toggle="tab" href="#maint" role="tab">Maintenance</a>
+								</li>
+								<li class="nav-item">
+									<img src="assets/images/icons/icon4.svg" alt="Repair"><a class="nav-link" data-toggle="tab" href="#repair" role="tab">Repair</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+							<!-- Tab panes -->
+							<div class="tab-content">
+								<div class="tab-pane fade show active" id="install" role="tabpanel">
+									<div class="container">
+										<div class="row">
+											<div class="col-md-6"><img class="img-fluid help-image" src="assets/images/banner-water-filtration.jpg" alt="Installation"></div>
+											<div class="col-md-6">
+												<h2 class="subtitle futuries-title">Water Filter Installation</h2>
+												<p>We have installed thousands of water purification systems in the greater New York City area and we can build and install the perfect water filter for your circumstances.</p>
+												<p>We have installed thousands of water purification systems in the greater New York City area and we can build and install the perfect water filter for your circumstances.</p>
+												<ul>
+													<li><strong>30 days Money Back Guarantee</strong></li>
+													<li>3 Years Warranty on All Filters</li>
+													<li>3 Years Warranty on All Other Parts</li>
+												</ul>
+											</div>
+											
+										</div>
+									</div>
+								</div>
+								<div class="tab-pane fade" id="maint" role="tabpanel" >
+									<div class="container">
+										<div class="row">
+											<div class="col-md-6"><img class="img-fluid help-image" src="assets/images/banner-water-filtration.jpg" alt="Installation"></div>
+											<div class="col-md-6">
+												<h2 class="subtitle futuries-title">Water Filter Installation</h2>
+												<p>We have installed thousands of water purification systems in the greater New York City area and we can build and install the perfect water filter for your circumstances.</p>
+												<p>We have installed thousands of water purification systems in the greater New York City area and we can build and install the perfect water filter for your circumstances.</p>
+												<ul>
+													<li><strong>30 days Money Back Guarantee</strong></li>
+													<li>3 Years Warranty on All Filters</li>
+													<li>3 Years Warranty on All Other Parts</li>
+												</ul>
+											</div>
+											
+										</div>
+									</div>
+								</div>
+								<div  class="tab-pane fade" id="repair" role="tabpanel">
+									<div class="container">
+										<div class="row">
+											<div class="col-md-6"><img class="img-fluid help-image" src="assets/images/banner-water-filtration.jpg" alt="Installation"></div>
+											<div class="col-md-6">
+												<h2 class="subtitle futuries-title">Water Filter Installation</h2>
+												<p>We have installed thousands of water purification systems in the greater New York City area and we can build and install the perfect water filter for your circumstances.</p>
+												<p>We have installed thousands of water purification systems in the greater New York City area and we can build and install the perfect water filter for your circumstances.</p>
+												<ul>
+													<li><strong>30 days Money Back Guarantee</strong></li>
+													<li>3 Years Warranty on All Filters</li>
+													<li>3 Years Warranty on All Other Parts</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Our Team Section -->
+		<section class="team section-padding section-dark">
+			<div class="container">
+					<div class="row">
+							<!-- Section Titile -->
+							<div class="col-md-12 wow animated fadeInLeft" data-wow-delay=".2s">
+									<div class="section-title">Our Team</div>
+							</div>
+					</div>
+					<div class="row">
+							<!-- Single Team Widget -->
+							<div class="col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp" data-wow-delay=".3s">
+									<div class="single-team-widget">
+											<img src="assets/images/team/team1.jpg" class="img-fluid" alt="">
+											<div class="team-member-info">
+													<div class="know-more">
+															<a class="btn btn-round btn-fab btn-xs" href="javascript:void(0)"><i class="material-icons mdi mdi-arrow-right"></i><div class="ripple-container"></div></a>
+													</div>
+													<h2 class="subtitle">Team Member 1</h2>
+													<p>Co-Founder</p>
+													<div class="social-profiles">
+														<a href="#"><i class="mdi mdi-twitter"></i></a>
+														<a href="#"><i class="mdi mdi-facebook"></i></a>
+														<a href="#"><i class="mdi mdi-linkedin"></i></a>
+													</div>
+											</div>
+									</div>
+							</div>
+							 <!-- Single Team Widget -->
+							<div class="col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp" data-wow-delay=".4s">
+									<div class="single-team-widget">
+											<img src="assets/images/team/team2.jpg" class="img-fluid" alt="">
+											<div class="team-member-info">
+													<div class="know-more">
+														<a class="btn btn-round btn-fab btn-xs" href="javascript:void(0)"><i class="material-icons mdi mdi-arrow-right"></i><div class="ripple-container"></div></a>
+													</div>
+													<h2 class="subtitle">Team Member 2</h2>
+													<p>Owner</p>
+													<div class="social-profiles">
+														<a href="#"><i class="mdi mdi-twitter"></i></a>
+														<a href="#"><i class="mdi mdi-facebook"></i></a>
+														<a href="#"><i class="mdi mdi-linkedin"></i></a>
+													</div>
+											</div>
+									</div>
+							</div>
+							 <!-- Single Team Widget -->
+							<div class="col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp" data-wow-delay=".5s">
+									<div class="single-team-widget">
+											<img src="assets/images/team/team3.jpg" class="img-fluid" alt="">
+											<div class="team-member-info">
+													<div class="know-more">
+															<a class="btn btn-round btn-fab btn-xs" href="javascript:void(0)"><i class="material-icons mdi mdi-arrow-right"></i><div class="ripple-container"></div></a>
+													</div>
+													<h2 class="subtitle">Team Member 3</h2>
+													<p>SEO Speacialist</p>
+													<div class="social-profiles">
+														<a href="#"><i class="mdi mdi-twitter"></i></a>
+														<a href="#"><i class="mdi mdi-facebook"></i></a>
+														<a href="#"><i class="mdi mdi-linkedin"></i></a>
+													</div>
+											</div>
+									</div>
+							</div>
+							<!-- Single Team Widget -->
+							<div class="col-md-6 col-lg-3 col-xl-3 wow animated fadeInUp" data-wow-delay=".6s">
+									<div class="single-team-widget">
+											<img src="assets/images/team/team1.jpg" class="img-fluid" alt="">
+											<div class="team-member-info">
+													<div class="know-more">
+															<a class="btn btn-round btn-fab btn-xs" href="javascript:void(0)"><i class="material-icons mdi mdi-arrow-right"></i><div class="ripple-container"></div></a>
+													</div>
+													<h2 class="subtitle">Team Member 4</h2>
+													<p>Head Of Ideas</p>
+													<div class="social-profiles">
+														<a href="#"><i class="mdi mdi-twitter"></i></a>
+														<a href="#"><i class="mdi mdi-facebook"></i></a>
+														<a href="#"><i class="mdi mdi-linkedin"></i></a>
+													</div>
+											</div>
+									</div>
+							</div>
+					</div>
+			</div>
+		</section>
+	<?php include('templates/counter.php');?>
+		<section class="Material-form-section section-padding section-dark">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 material-title-section wow animated fadeInLeft" data-wow-delay=".2s">
+						<div class="section-title">Free Water Test</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-12">
+									<form class="shake" role="form" method="post" id="contactForm3" name="contact-form3" data-toggle="validator">
+											<!-- Name -->
+											<div class="form-group label-floating">
+												<label class="control-label" for="name3">Name</label>
+												<input class="form-control" id="name3" type="text" name="name3" required data-error="Please enter your name">
+												<div class="help-block with-errors"></div>
+											</div>
+											<!-- email -->
+											<div class="form-group label-floating">
+												<label class="control-label" for="email3">Email</label>
+												<input class="form-control" id="email3" type="email" name="email3" required data-error="Please enter your Email">
+												<div class="help-block with-errors"></div>
+											</div>
+											<!-- Subject -->
+											<div class="form-group label-floating">
+												<label class="control-label">Phone</label>
+												<input class="form-control" id="msg_subject3" type="text" name="subject3" required data-error="Please enter your message subject">
+												<div class="help-block with-errors"></div>
+											</div>
+											<!-- Form Submit -->
+											<div class="form-submit mt-5">
+													<button class="btn btn-common" type="submit" id="form-submit3"><i class="material-icons mdi mdi-message-outline"></i> Get Your Free Consultation</button>
+													<div id="msgSubmit" class="h3 text-center hidden"></div>
+													<div class="clearfix"></div>
+											</div>
+									</form>
+								 </div>
+								</div>
+							</div>
+		</section>
+		<!-- About Section End -->
+		<section class="Material-contact-section section-padding">
+			<div class="container">
+					<div class="row">
+						<div class="col-md-12 material-title-section wow animated fadeInLeft" data-wow-delay=".2s">
+							<div class="section-title">New York</div>
+						</div>
+					</div>
+					<div class="row mb-5">
+							<!-- Section Titile -->
+							<div class="col-md-5 mt-3 contact-widget-section2 wow animated fadeInLeft" data-wow-delay=".2s">
+								<p>The citizens of large metropolitan areas, such as New York, suffer from low water quality more than those who live in the countryside. How can we protect ourselves from dirty water entering our everyday life? The answer is water purifying filters.</p>
+								<p>Being the leader of the New York water filter installation business, we cordially invite you to visit our New York store and choose a water purifying system that will suit your lifestyle and budget.</p>
+								<p>Our specialists will advise you on systems and cartridges and make an appointment for one of our experts to come and install your new home source of tasty, purified water. High Water Standard is waiting for your visit, so tomorrow you can enjoy a clean and healthy water source anywhere you want!</p>
+								<div class="find-widget">
+									<a href="#"><i class="material-icons mdi mdi-map-marker"></i>1938 Stillwell Ave, Brooklyn, NY 11223</a>
+								</div>
+								<div class="find-widget">
+										<a href="#"><i class="material-icons mdi mdi-phone"></i> 718-407-9357</a>
+								</div>
+								<div class="find-widget">
+										<a href="#"><i class="material-icons mdi mdi-email-open mr-3"></i> hws@gmail.com</a>
+								</div>
+								<div class="find-widget">
+										<a href="#"><i class="material-icons mdi mdi-clock"></i> Mon to Sat: 09:30 AM - 10.30 PM</a>
+								</div>
+							</div>
+							<div class="col-md-7 mt-3 wow animated fadeInRight" data-wow-delay=".2s">
+								<img style="box-shadow: 0 1px 4px rgba(0,0,0,0.26);" src="assets/images/map.png" alt="map" class="img-fluid">
+							</div>
+
+							<!-- contact form -->
+					</div>
+					<div class="row">
+						<div class="col-md-12 material-title-section wow animated fadeInLeft" data-wow-delay=".2s">
+							<div class="section-title">New Jersey</div>
+						</div>
+					</div>
+					<div class="row mb-5">
+							<!-- Section Titile -->
+							<div class="col-md-5 mt-3 contact-widget-section2 wow animated fadeInLeft" data-wow-delay=".2s">
+								<p>Visit our High Water Standard store in New Jersey! Come and choose any water purifying system at a comfortable price. We will provide free consultation on any water filter framework installation, repair, and support.</p>
+								<p>High Water Standard specialists will provide you with a free in-home water test and assure you the best quality home drinking water and fast installation service. We are happy to present you with highly qualified expert help in the installation of water purification systems of any complexity.</p>
+								<p>We will answer any of your inquiries and give useful, valuable data that will enable you to settle on the correct water filter decision. Make your first step on the way to a healthy lifestyle, caring about nature, and conscious consumption with us!</p>
+								<div class="find-widget">
+									<a href="#"><i class="material-icons mdi mdi-map-marker"></i>1938 Stillwell Ave, Brooklyn, NY 11223</a>
+								</div>
+								<div class="find-widget">
+										<a href="#"><i class="material-icons mdi mdi-phone"></i> 718-407-9357</a>
+								</div>
+								<div class="find-widget">
+										<a href="#"><i class="material-icons mdi mdi-email-open mr-3"></i> hws@gmail.com</a>
+								</div>
+								<div class="find-widget">
+										<a href="#"><i class="material-icons mdi mdi-clock"></i> Mon to Sat: 09:30 AM - 10.30 PM</a>
+								</div>
+							</div>
+							<div class="col-md-7 mt-3 wow animated fadeInRight" data-wow-delay=".2s">
+								<img style="box-shadow: 0 1px 4px rgba(0,0,0,0.26);" src="assets/images/map.png" alt="map" class="img-fluid">
+							</div>
+
+							<!-- contact form -->
+					</div>
+			</div>
+		</section>
+
+		<!-- Call to action Section -->
+		<section class="call-to-action-section">
+			<div class="container">
+				<div class="row justify-content-between">
+					<div class="col-md-8 col-lg-8 col-xs-12 text-left wow animated fadeInLeft" data-wow-delay=".2s">
+							<div class="h1">Call Us 718-407-9357</div>
+							<h3>Enjoy our 24/7 support</h3>
+					</div>
+					<div class="col-md-4 col-lg-4 col-xs-12 mt-4 text-right wow animated fadeInRight" data-wow-delay=".3s">
+							<a href="javascript:void(0)" class="animated4 btn btn-border"><i class="material-icons mdi mdi-phone"></i> Contact us</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Call to action Section End -->
+
+<?php include('templates/footer.php');?>
